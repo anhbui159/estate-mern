cccbb3718766b9b13462e3f341bbe5a1fe2d4171
anhbui159 <anhbui159@gmail.com>
2024-09-13 22:11:42 +0700

    Finished update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,34 @@
+import { errorHandler } from '../utils/error.js';
+import bcryptjs from 'bcryptjs'
+import User from "../models/user.model.js"
+
 export const test = (req, res) => {
     res.json({
-        message: "Route testint",
+        message: "Route testing",
     })
+}
+
+export const updateUser = async (req, res,next) => 
+{
+    if(req.user.id !== req.params.id){
+        return next(errorHandler(401, "Can only update your account"))
+    }
+    try{
+        if(req.body.password){
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+            $set:{
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                photo: req.body.photo,
+            }
+        }, {new:true})
+        const {password, ...info} = updatedUser._doc;
+
+        res.status(200).json(info);
+    } catch (error){
+        next(error)
+    }
 }
